0f1a23101a96bee5e5a878b73f22142ba3864a9b
deepsth <infodipstha@gmail.com>
2025-02-18 21:55:23 +0545

    slicing all done some responsive left to be done

--- assests/js/main.js
+++ assests/js/main.js
@@ -1,39 +1,52 @@
 console.log('code running');
-$('.carousel').carousel();
-function t(t, e) {
-    t.each(function () {
-        var t = $(this),
-            i = "animated " + t.data("animation-" + e);
-        t.addClass(i).one("webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend", function () {
-            t.removeClass(i);
-        });
+$(document).ready(function (){
+    // Declare Carousel jquery object
+    var owl = $('.owl-carousel');
+    var owlContent = $('#owl-content');
+  
+    // Carousel content initialization
+    owlContent.owlCarousel({
+        loop:true,
+        margin:0,
+        autoplaySpeed: 3000,
+        navSpeed:3000,
+        nav:false,
+        autoplay: true,
+        rewind: true,
+        items:1,
+        animateIn: 'fadeIn',
+        animateOut: 'fadeOut',
+        
+ 
     });
-}
-function e(t) {
-    var e = $(document).scrollTop();
-    $(".navbar-nav a").each(function () {
-        var t = $(this),
-            i = $(t.attr("href"));
-        i.position().top <= e && i.position().top + i.height() > e ? ($(".navbar-nav a").removeClass("current-item"), t.addClass("current-item")) : t.removeClass("current-item");
+    // Carousel initialization
+    owl.owlCarousel({
+        loop:true,
+        margin:0,
+        autoplaySpeed: 3000,
+        navSpeed:3000,
+        nav:true,
+        autoplay: true,
+        rewind: true,
+        animateIn: 'fadeIn',
+        animateOut: 'fadeOut',
+        items:1
     });
-}
-var i = $("#img-layout");
-    i.owlCarousel({
-        loop: !0,
-        margin: 50,
-        navSpeed: 500,
-        autoplayHoverPause: !1,
-        nav: !0,
-        animateIn: "fadeIn", 
-        autoplay: !0,
-        autoplayTimeout: 4e3,
-        items: 1,
-        responsiveClass: !0,
-        responsive: { 0: { items: 1, nav: !1 }, 600: { items: 2, nav: !1 }, 768: { items: 3, nav: !0 }, 1e3: { items: 4, nav: !0, loop: !0 }, 1200: { items: 4, nav: !0, loop: !0 } },
-    }),
-    i.on("change.owl.carousel", function (e) {
-        t($(".owl-item", i).eq(e.item.index).find("[data-animation-out]"), "out");
-    }),
-    i.on("changed.owl.carousel", function (e) {
-        t($(".owl-item", i).eq(e.item.index).find("[data-animation-in]"), "in");
-    })
+  
+  });
+
+
+    let NavTabLink = document.querySelectorAll('.nav-tabs .nav-link');
+
+      NavTabLink.forEach(tab => {
+        tab.addEventListener('click', function(){
+          const tabContent = this.closest('.section-search').querySelector('.tab-content');
+         if(tabContent){
+          if(this.classList.contains('make')){
+
+            tabContent.classList.add('show');
+          } else tabContent.classList.remove('show');
+         }
+         
+        })
+      })
