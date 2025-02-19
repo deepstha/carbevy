1c49e44b641adc90d0f4c8ae8df89ee773d7fe3a
deepsth <infodipstha@gmail.com>
2025-02-19 13:33:00 +0545

    responsive fixed for mobile tablet and desktop version

--- assests/js/main.js
+++ assests/js/main.js
@@ -1,52 +1,27 @@
-console.log('code running');
-$(document).ready(function (){
-    // Declare Carousel jquery object
-    var owl = $('.owl-carousel');
-    var owlContent = $('#owl-content');
-  
-    // Carousel content initialization
-    owlContent.owlCarousel({
-        loop:true,
-        margin:0,
-        autoplaySpeed: 3000,
-        navSpeed:3000,
-        nav:false,
-        autoplay: true,
-        rewind: true,
-        items:1,
-        animateIn: 'fadeIn',
-        animateOut: 'fadeOut',
-        
- 
-    });
-    // Carousel initialization
-    owl.owlCarousel({
-        loop:true,
-        margin:0,
-        autoplaySpeed: 3000,
-        navSpeed:3000,
-        nav:true,
-        autoplay: true,
-        rewind: true,
-        animateIn: 'fadeIn',
-        animateOut: 'fadeOut',
-        items:1
-    });
-  
-  });
 
+// Toggle class on tab-content on each tab click
+let NavTabLink = document.querySelectorAll('.nav-tabs .nav-link');
+NavTabLink.forEach(tab => {
+  tab.addEventListener('click', function(){
+    const tabContent = this.closest('.section-search').querySelector('.tab-content');
+    if(tabContent){
+    if(this.classList.contains('make')){
 
-    let NavTabLink = document.querySelectorAll('.nav-tabs .nav-link');
+      tabContent.classList.add('show');
+    } else tabContent.classList.remove('show');
+    }
+    
+  })
+})
 
-      NavTabLink.forEach(tab => {
-        tab.addEventListener('click', function(){
-          const tabContent = this.closest('.section-search').querySelector('.tab-content');
-         if(tabContent){
-          if(this.classList.contains('make')){
+// Header top section clode button
+let btnClose = document.querySelector('.btn-close');
 
-            tabContent.classList.add('show');
-          } else tabContent.classList.remove('show');
-         }
-         
-        })
-      })
+btnClose.addEventListener('click', function(){
+  let headerTop = document.querySelector('.header-top');
+  let themeContainer = document.querySelector('.theme-container');
+  headerTop.classList.add('hide');
+  themeContainer.classList.add('hide');
+  console.log('hide')
+
+})
